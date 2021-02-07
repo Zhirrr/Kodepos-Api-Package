3f867cfdf9a3846951ae4bc82b5d468b297a80a8
Zhirrr <75620279+Zhirrr@users.noreply.github.com>
2021-02-07 19:02:12 +0700

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-/**
- * @author Zahir Hadi Athallah <zhirrrstudio@gmail.com>
- * @license MIT
- */
-
- const KisahNabi = require('./utils')
-
- KisahNabi.searchNabi('kisahnabi+scraper')
-    .then(data => console.log(data))
